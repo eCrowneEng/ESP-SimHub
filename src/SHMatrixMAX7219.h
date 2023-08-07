3eff8be690828511e00b541f6ff466f2ef47cbe7
Ecor <me@ecor.me>
2023-08-07 16:21:05 -0700

    Fix support for MAX 7 segment modules (#8)
    * support

--- src/SHMatrixMAX7219.h
+++ src/SHMatrixMAX7219.h
@@ -1,34 +1,33 @@
 #ifndef _SHMATRIXMAX7219_H__
 #define _SHMATRIXMAX7219_H__
 #include <Arduino.h>
-#include <MD_MAX72xx.h>
-
-#define HARDWARE_TYPE MD_MAX72XX::GENERIC_HW
+#include <LedController.hpp>
 
 class SHMatrixMAX7219 {
 private:
-	MD_MAX72XX mx;
 	byte MAX7221_MATRIX_LUMINOSITY = 0;
+	LedController MAX7221;
 public:
-
-	SHMatrixMAX7219(int DataPin, int ClkPin, int CsPin) : mx(HARDWARE_TYPE, CsPin, 1) {}
-
-	void begin() {
-		mx.begin();
-		mx.clear(0, 1);
+	void begin(int dataPin, int clkPin, int csPin) {
+		MAX7221.init(dataPin, clkPin, csPin, 1);
+		for(unsigned int i = 0; i < MAX7221.getSegmentCount(); i++) {
+			for(unsigned int j = 0; j < 8; j++) {
+				MAX7221.setRow(i, j, 0x00);
+			}
+		}
+		MAX7221.setIntensity(15);
 	}
 
 	void read()
 	{
-		// Wait for display data
 		int newIntensity = FlowSerialTimedRead();
 		if (newIntensity != MAX7221_MATRIX_LUMINOSITY) {
-			mx.control(MD_MAX72XX::INTENSITY, min(MAX_INTENSITY, newIntensity));
+			MAX7221.setIntensity(newIntensity);
 			MAX7221_MATRIX_LUMINOSITY = newIntensity;
 		}
 
 		for (int j = 0; j < 8; j++) {
-			mx.setRow(7-j, FlowSerialTimedRead());
+			MAX7221.setRow(0, 7 - j, FlowSerialTimedRead());
 		}
 	}
 };
